a8d00abdef20a693cf717d4348c6b09bbd5b3ce4
elliot-ball <elliot_ball@bits.uk.com>
2014-09-05 16:58:40 +0100

    undo add ajax.groups call

--- www/js/application.js
+++ www/js/application.js
@@ -837,7 +837,6 @@ function ReturnBlob( data ){
 									for (var i = 0; i < result.length; i++) {
 										Groups.push ( result[i] );
 									};
-									ajax.groups();
 									GetFirstGroup();
 
 									setTimeout(function() {
