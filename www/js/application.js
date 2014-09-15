112c07d058838a53c0730d4903714f9394cc1b79
elliot-ball <elliot_ball@bits.uk.com>
2014-09-15 15:11:51 +0100

    log var map

--- www/js/application.js
+++ www/js/application.js
@@ -992,6 +992,8 @@ function ReturnBlob( data ){
 		},
 		maps: function(e){
 			var map = CurrentMap.Path.substr( CurrentMap.Path.lastIndexOf('/')+1), fullpath = '';
+			AddMessage("map = " map, "long", "top");
+
 			window.requestFileSystem( RequestLocalSystem(), RequestSize, function ( fs ){
 				fs.root.getDirectory("OSMobile/maps", {create:true}, function ( de ){
 					fullpath = de.toURL();
