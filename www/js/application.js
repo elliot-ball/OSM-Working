ec19c6240e0e45dbe6f3be6a42c12e27799cd2bf
elliot-ball <elliot_ball@bits.uk.com>
2014-10-02 15:40:53 +0100

    test

--- www/js/application.js
+++ www/js/application.js
@@ -209,7 +209,8 @@ try{
 			$('#DeviceImageDisplay').attr("novis", "");
 		},
 		loadImage: function( path ){
-			$('#DeviceImageDisplay>display.content').empty().append("<img/>");
+			// $('#DeviceImageDisplay>display.content').empty().append("<img/>");
+			$('#DeviceImageDisplay>display.content').append("<img/>");
 			var i = $('#DeviceImageDisplay>display.content>img');
 
 			var img = new Image();
@@ -712,6 +713,7 @@ var Ajax ={
 					}, File.error);
 				},File.error);
 			}, 100);
+			$('#DeviceImageDisplay>display.content').append("<h3>WOO HOO!</h3>");
 		}
 	};
 
